4226b8190e38770a74c3fcbe9e0373bea72b3890
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-14 13:51:47 +0530

    Added file input and track selection functions

--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef, useState, useContext } from "react";
+import Track from "../Track/Track";
+import { AudioFilesContext } from "../../contexts/AudioFilesContext";
 
 function Playlist() {
+  const audioFileContext = useContext(AudioFilesContext);
+  const fileInput = useRef<HTMLInputElement>(null);
+
+  function onTrackSelect(e: React.ChangeEvent<HTMLInputElement>) {
+    const selectedFileCount = e.target.files!.length;
+    const selectedFiles = e.target.files!;
+
+    const newFiles: Array<File> = [];
+
+    for (let i = 0; i < selectedFileCount; i++) {
+      newFiles.push(selectedFiles[i]);
+    }
+
+    audioFileContext.setAudioFiles!([
+      ...audioFileContext.trackFiles!,
+      ...newFiles,
+    ]);
+  }
+
   return (
-    <div className="grow shrink basis-[50%] w-auto h-full border-2 border-white bg-white/50 rounded-lg "></div>
+    <div className="grow shrink basis-[50%] w-auto h-full flex flex-col items-start justify-start bg-white rounded-lg ">
+      <input
+        ref={fileInput}
+        type="file"
+        name="audio_files"
+        id="audio_file_input"
+        accept="audio/*"
+        multiple={true}
+        onChange={(e) => {
+          onTrackSelect(e);
+        }}
+        className="shrink grow basis-[8%] w-full max-h-[50px] h-fit p-2 border-b-2 border-gray-500 text-gray-500"
+      />
+      {/* TRACK CONTAINER */}
+      <div className="shrink grow basis-[90%] w-full h-full max-h-[280px] overflow-y-scroll">
+        {/* TRACK */}
+        {audioFileContext.trackFiles?.map((track, index) => {
+          return (
+            <Track
+              key={`track_${Math.random()}`}
+              trackName={track.name}
+              index={index}
+              length={track.size}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 }
 
