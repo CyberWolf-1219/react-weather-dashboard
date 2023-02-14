6cd49aaacb7643cd5780bcb429be383cfce353c2
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-14 13:48:41 +0530

    Removed Aspect Ratio

--- src/components/AudioVisualizer/AudioVisualizer.tsx
+++ src/components/AudioVisualizer/AudioVisualizer.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 function AudioVisualizer() {
-  return (
-    <div className="aspect-[1/1] w-full max-w-[300px] h-auto bg-white/50 rounded-lg"></div>
-  );
+  return <div className="w-full h-full bg-white rounded-lg"></div>;
 }
 
 export default AudioVisualizer;
