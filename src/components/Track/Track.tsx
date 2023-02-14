3fa88a48657120a8a8936b9aa7f614a40a0e86a5
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-14 13:46:25 +0530

    Created Track Component

---
+++ src/components/Track/Track.tsx
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import { BsPlayCircle } from "react-icons/bs";
+import { AudioFilesContext } from "../../contexts/AudioFilesContext";
+
+interface ITrack {
+  index: number;
+  trackName: string;
+  artist?: string;
+  length: number;
+}
+
+function Track(props: ITrack) {
+  const audioFileContext = useContext(AudioFilesContext);
+
+  function play(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    const fileToPlay = audioFileContext.trackFiles![props.index];
+    audioFileContext.setCurrentFile!(fileToPlay);
+    audioFileContext.play();
+  }
+
+  return (
+    <div className="w-full h-fit p-2  text-black bg-white">
+      <div className="flex flex-row items-center justify-between">
+        <span className="grow shrink basis-[90%] flex flex-col">
+          <b className="w-full max-w-[40ch] h-fit min-h-[30px] overflow-hidden truncate">
+            {props.trackName}
+          </b>
+          <div className="flex flex-row items-center justify-start gap-4">
+            <span>
+              <small>Artist: {props.artist ?? "Unknown"}</small>
+            </span>
+            <span className="">
+              <small>Length: {props.length}</small>
+            </span>
+          </div>
+        </span>
+        <span className="grow shrink basis-[10%]">
+          <button onClick={play}>
+            <BsPlayCircle size={`2.5rem`} color="black" />
+          </button>
+        </span>
+      </div>
+      <hr className="w-full h-auto border-[1.5px] " />
+    </div>
+  );
+}
+
+export default Track;
