7fc86832a5fd08b324c73d29c1ae365346e14df9
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-14 13:52:52 +0530

    Removed Spotify component & changed styles

--- src/components/MusicPlayer/MusicPlayer.tsx
+++ src/components/MusicPlayer/MusicPlayer.tsx
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import AudioVisualizer from "../AudioVisualizer/AudioVisualizer";
 import ControlsPanel from "../ControlsPanel/ControlsPanel";
 import Playlist from "../Playlist/Playlist";
-import SpotifySearchPanel from "../SpotifySearchPanel/SpotifySearchPanel";
+import FileInfoPanel from "../FileInfoPanel/FileInfoPanel";
 
 function MusicPlayer() {
   return (
-    <div className="grow shrink basis-[65%] w-full h-full p-4 flex flex-col items-center justify-start gap-4  border-2 border-red-500 bg-white/30 rounded-lg">
+    <div className="grow shrink basis-[65%] w-full h-full p-4 flex flex-col items-center justify-start gap-4  bg-white/30 rounded-lg">
       {/* TOP COMPONENTS WRAPPER */}
       <div className="w-full h-fit flex flex-row items-end justify-start gap-4">
         {/* AUDIO VISUALIZER */}
         <AudioVisualizer />
-        {/* CONTROLLS */}
-        <ControlsPanel />
+        <div className="w-full h-full flex flex-col items-center justify-start gap-4">
+          {/* FILE INFO PANEL */}
+          <FileInfoPanel />
+          {/* CONTROLLS */}
+          <ControlsPanel />
+        </div>
       </div>
       {/* BOTTOM COMPONENTS WRAPPER */}
       <div className="w-full h-full flex flex-row items-start justify-center gap-4">
-        {/* SPOTIFY SEARCH */}
-        <SpotifySearchPanel />
         {/* PLAYLIST */}
         <Playlist />
       </div>
