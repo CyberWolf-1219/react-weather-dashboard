67c1fbc509d6a29d94fda6970b864e3fd66a1b40
CyberWolf-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-09-20 13:18:03 +0530

    memoized

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from 'react';
 
 const bg_videos = [
-  { lg: "/Nature.mp4", sm: "MOBILE-Nature.mp4" },
-  { lg: "/Mount-Fuji.mp4", ms: "/MOBILE-Mount-Fuji.mp4" },
-  { lg: "/Raining-Outside.mp4", sm: "/MOBILE-Raining-Outside.mp4" },
+  { lg: '/Nature.mp4', sm: 'MOBILE-Nature.mp4' },
+  { lg: '/Mount-Fuji.mp4', ms: '/MOBILE-Mount-Fuji.mp4' },
+  { lg: '/Raining-Outside.mp4', sm: '/MOBILE-Raining-Outside.mp4' },
 ];
 
 const video = bg_videos[Math.floor(Math.random() * 3)].lg;
@@ -16,16 +16,17 @@ function Background() {
         muted={true}
         controls={false}
         loop={true}
-        className={`w-full h-full object-cover`}
-      >
-        <source src={video} type="video/mp4" />
+        className={`w-full h-full object-cover`}>
+        <source
+          src={video}
+          type='video/mp4'
+        />
       </video>
       {/* OVERLAY */}
       <div
-        className={`absolute z-[5] inset-0 w-full h-full backdrop-blur-[0.5px]`}
-      ></div>
+        className={`absolute z-[5] inset-0 w-full h-full backdrop-blur-[0.5px]`}></div>
     </div>
   );
 }
 
-export default Background;
+export default React.memo(Background);
